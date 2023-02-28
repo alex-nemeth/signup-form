b7e2b8d2d2125cd94ea0fed15f90cb3b5cc60855
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-02-28 13:54:11 +0100

    all input checks functional

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
 const submitButton = document.querySelector(".submit");
 submitButton.addEventListener("click", checkMatch);
 
+const firstName = document.querySelector(".firstname");
+const lastName = document.querySelector(".lastname");
+
+const email = document.querySelector(".email");
+
 const passOne = document.querySelector(".password");
 const passTwo = document.querySelector(".confirmpassword");
 
-const alertText = document.querySelector(".alert");
+const nameAlert = document.querySelector("#namealert");
+const mailAlert = document.querySelector("#emailalert");
+const pwAlert = document.querySelector("#pwalert");
 
 document.querySelector(".form").addEventListener("submit", (e) => {
-    // on form submission, prevent default
     e.preventDefault();
-
-    // construct a FormData object, which fires the formdata event
     new FormData(document.querySelector(".form"));
 });
 
 document.querySelector(".form").addEventListener("formdata", (e) => {
     let fName = e.formData.get("firstname");
     let lName = e.formData.get("lastname");
-    let email = e.formData.get("email");
+    let mail = e.formData.get("email");
     let pwOne = e.formData.get("password");
     let pwTwo = e.formData.get("confirmpassword");
     pwCheck(pwOne, pwTwo);
+    nameCheck(fName, lName);
+    emailCheck(mail);
 });
 
 function checkMatch(e) {
@@ -28,36 +34,49 @@ function checkMatch(e) {
     e.preventDefault();
 }
 
-function pwCheck(pwOne, pwTwo) {
-    if (pwOne !== pwTwo) {
-        alertText.textContent = "Passwords do not match.";
-        highlightPw();
-    } else if (pwOne.length < 6 || pwTwo.length < 6) {
-        alertText.textContent = "Password must be at least 6 symbols long.";
-        highlightPw();
-    } else {
-        alertText.textContent = "";
-        hideHighlight();
-    }
-}
-
 function nameCheck(fName, lName) {
     if (fName.length === 0 || lName.length === 0) {
+        nameAlert.textContent = "Please fill out your name.";
+        highlight(firstName, lastName, nameAlert);
+    } else {
+        nameAlert.textContent = "";
+        hideHighlight(firstName, lastName);
     }
 }
 
-function highlightPw() {
-    passOne.style.borderColor = "red";
-    passTwo.style.borderColor = "red";
+function emailCheck(mail) {
+    if (!mail.includes("@")) {
+        mailAlert.textContent = "Please provide a real e-mail address.";
+        highlight(email, email, mailAlert);
+    } else {
+        mailAlert.textContent = "";
+        hideHighlight(email, email);
+    }
+}
+
+function pwCheck(pwOne, pwTwo) {
+    if (pwOne !== pwTwo) {
+        pwAlert.textContent = "Passwords do not match.";
+        highlight(passOne, passTwo, pwAlert);
+    } else if (pwOne.length < 6 || pwTwo.length < 6) {
+        pwAlert.textContent = "Password must be at least 6 symbols long.";
+        highlight(passOne, passTwo, pwAlert);
+    } else {
+        pwAlert.textContent = "";
+        hideHighlight(passOne, passTwo);
+    }
+}
+
+function highlight(elementOne, elementTwo, alertText) {
+    elementOne.style.borderColor = "red";
+    elementTwo.style.borderColor = "red";
     alertText.style.display = "block";
 }
 
-function hideHighlight() {
-    passOne.style.borderColor = "rgb(37, 37, 37)";
-    passTwo.style.border = "rgb(37, 37, 37)";
+function hideHighlight(elementOne, elementTwo) {
+    elementOne.style.borderColor = "rgb(37, 37, 37)";
+    elementTwo.style.border = "rgb(37, 37, 37)";
 }
 
-//make alert frame go away on good input
 //maybe make good notification if inputs are right
-//implement alert frames for all the other elements
 //fix the css on alert text
